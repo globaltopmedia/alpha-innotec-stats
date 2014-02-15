4890650fa9738f3f084c474a8c8b945168798627
Marcel Dumont <marcel@globaltopmedia.com>
2014-02-15 20:06:42 +0000

    updated ip

--- includes/config.php
+++ includes/config.php
@@ -1,9 +1,9 @@
 <?php
 // connection to heatpump management module
-$IpWwc = "192.168.178.252";
+$IpWwc = "192.168.178.38";
 $WwcJavaPort = "8888";
 // URL (location) of ycharts
 $YCURL = "/yui3";
 // reload time for chart*.php
-$timeframe=86400*2;
+$timeframe=86400*7; // 7 days of data
 ?>
